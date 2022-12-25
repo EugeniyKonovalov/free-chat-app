2b477dd50e8336fec1e55fbef8c9c0bc296dd3f0
EvgeniyKonovalov <cicerron2020@gmail.com>
2022-12-25 14:26:32 +0200

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOI1iWH47W0T3D8qhINsAlDpJjXoVoHic",
-  authDomain: "chat-new-3f4a0.firebaseapp.com",
-  databaseURL: "https://chat-new-3f4a0-default-rtdb.firebaseio.com",
-  projectId: "chat-new-3f4a0",
-  storageBucket: "chat-new-3f4a0.appspot.com",
-  messagingSenderId: "616089502927",
-  appId: "1:616089502927:web:f82a94e2f16bc78b6823ba",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
